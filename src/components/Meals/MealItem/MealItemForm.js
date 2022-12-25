21559293bcad6694250c6ae766f02261a23a269e
Asmaa <asmaakhdr24@gmail.com>
2022-12-25 17:17:10 +0200

    add to cart form

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Input from '../../UI/Input';
+import classes from './MealItemForm.module.css';
 
 const MealItemForm = () => {
-  return <div>MealItemForm</div>;
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{ id: 'amount', type: 'number', min: '1', max: '5', step: '1', defaultValue: '1' }}
+      />
+      <button>Add +</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
