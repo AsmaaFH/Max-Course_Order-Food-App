ac24c6d48f9259570dfbed1c576261e68c7ddc74
Asmaa <asmaakhdr24@gmail.com>
2022-12-25 12:15:34 +0200

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import Hearder from './components/Layout/Hearder';
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <>
+      <Hearder></Hearder>
+    </>
   );
 }
 
