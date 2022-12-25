08d5893beff361ad81f42fd85b1530e75436762e
Asmaa <asmaakhdr24@gmail.com>
2022-12-25 14:55:21 +0200

    adding meals compinent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hearder from './components/Layout/Hearder';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <>
       <Hearder></Hearder>
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Fragment } from 'react';
+
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
