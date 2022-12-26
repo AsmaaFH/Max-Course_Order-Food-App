9c8e4390e5f5dbda0b56240c8073279acf44ca43
Asmaa <asmaakhdr24@gmail.com>
2022-12-26 23:21:31 +0200

    remove item

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -32,6 +32,24 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
 };
 
 const CartProvider = (props) => {
@@ -41,7 +59,9 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'ADD', item: item });
   };
 
-  const removeItemFronCartHandler = (id) => {};
+  const removeItemFronCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  };
 
   const cartContext = {
     items: cartState.items,
